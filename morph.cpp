5414a9a80f7622526c4bd35fbeffe7e8102d22c6
ql69 <ql69@njit.edu>
2016-08-30 17:37:29 -0400

    hello world

--- morph.cpp
+++ morph.cpp
@@ -1,8 +1,10 @@
 #include <opencv2/core.hpp>
+#include <iostream>
 
 using namespace std;
+using namespace cv;
 
 int main(int argc, char* argv[])
 {
-	
+	cout << "hello world " << CV_VERISON << endl;
 }
